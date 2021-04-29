669543d1c1ac8cbf3f584ce9ef10563db412a1c4
vtm9 <vtmilyakov@yandex.ru>
2021-04-29 14:22:03 +0300

    Add render rectangle with all cases and tests

--- lib/canvas/render.ex
+++ lib/canvas/render.ex
@@ -1,13 +1,16 @@
 defmodule Canvas.Render do
   @moduledoc false
 
-  alias Canvas.Render
+  alias __MODULE__
 
   @default_char " "
 
   @typep points :: %{optional({pos_integer, pos_integer}) => atom}
   @type t :: %Render{points: points, width: pos_integer, height: pos_integer}
   defstruct points: %{}, width: 0, height: 0
+
+  defguardp in_image?(width, height, x, y)
+            when x >= 0 and x <= width and y >= 0 and y <= height
 
   def call(image) do
     %{drawings: drawings, width: width, height: height} = image
@@ -24,28 +27,103 @@ defmodule Canvas.Render do
 
   defp render_drawings(render, drawings) do
     Enum.reduce(drawings, render, fn drawing, acc ->
-      case drawing do
-        %{type: "rectangle"} -> render_rectangle(acc, drawing)
-        %{type: "flood"} -> render_flood(acc, drawing)
-      end
+      render_drawing(acc, drawing)
     end)
   end
 
-  defp render_rectangle(render, rectangle) do
+  defp render_drawing(
+         render,
+         %{type: "rectangle", props: %{outline: outline, fill: fill}} = drawing
+       )
+       when outline != nil and fill != nil do
+    %{
+      props: %{
+        corner: [corner_x, corner_y],
+        width: width,
+        height: height
+      }
+    } = drawing
+
+    (corner_x + 1)..(corner_x + width - 2)
+    |> Enum.reduce(render, fn x, acc ->
+      render_column(acc, x, corner_y, corner_y + height - 1, fill)
+    end)
+    |> render_rectangle_outline(drawing)
+  end
+
+  defp render_drawing(
+         render,
+         %{type: "rectangle", props: %{outline: outline, fill: fill}} = drawing
+       )
+       when outline != nil and fill == nil do
+    render
+    |> render_rectangle_outline(drawing)
+  end
+
+  defp render_rectangle_outline(render, drawing) do
+    %{
+      props: %{
+        corner: [corner_x, corner_y],
+        outline: outline,
+        width: width,
+        height: height
+      }
+    } = drawing
+
+    render
+    |> render_row(corner_y, corner_x, corner_x + width - 1, outline)
+    |> render_row(corner_y + height - 1, corner_x, corner_x + width - 1, outline)
+    |> render_column(corner_x, corner_y, corner_y + height - 1, outline)
+    |> render_column(corner_x + width - 1, corner_y, corner_y + height - 1, outline)
+  end
+
+  defp render_drawing(
+         render,
+         %{type: "rectangle", props: %{outline: outline, fill: fill}} = drawing
+       )
+       when outline == nil and fill != nil do
+    %{
+      props: %{
+        corner: [corner_x, corner_y],
+        width: width,
+        height: height
+      }
+    } = drawing
+
+    corner_x..(corner_x + width - 1)
+    |> Enum.reduce(render, fn x, acc ->
+      render_column(acc, x, corner_y, corner_y + height - 1, fill)
+    end)
+  end
+
+  defp render_drawing(render, %{type: "flood"} = flood) do
     render
   end
 
-  defp render_flood(render, flood) do
-    render
+  def render_row(render, y, from_x, to_x, char) do
+    from_x..to_x
+    |> Enum.reduce(render, fn x, acc -> render_point(acc, {x, y}, char) end)
   end
+
+  def render_column(render, x, from_y, to_y, char) do
+    from_y..to_y
+    |> Enum.reduce(render, fn y, acc -> render_point(acc, {x, y}, char) end)
+  end
+
+  def render_point(%{width: width, height: height, points: points} = render, {x, y}, char)
+      when in_image?(width, height, x, y) do
+    %{render | points: Map.put(points, {x, y}, char)}
+  end
+
+  def render_point(render, _point, _char), do: render
 
   defp print(render) do
     %Render{width: width, height: height} = render
 
-    Enum.reduce(1..height, "", fn row_num, acc ->
+    Enum.reduce(0..height, "", fn row_num, acc ->
       row =
-        Enum.reduce(1..width, acc, fn col_num, col_acc ->
-          col_acc <> Map.get(render, {row_num, col_num}, @default_char)
+        Enum.reduce(0..width, acc, fn col_num, col_acc ->
+          col_acc <> Map.get(render.points, {col_num, row_num}, @default_char)
         end)
 
       if row_num == height do
--- test/canvas/render_test.exs
+++ test/canvas/render_test.exs
@@ -27,20 +27,79 @@ defmodule Canvas.RenderTest do
           }
         )
 
-      image = build(:image, width: 24, height: 9, drawings: [rec1, rec2])
-      expected = ~S(
+      image = build(:image, width: 23, height: 8, drawings: [rec1, rec2])
 
-
-   @@@@@
-   @XXX@  XXXXXXXXXXXXXX
-   @@@@@  XOOOOOOOOOOOOX
-          XOOOOOOOOOOOOX
-          XOOOOOOOOOOOOX
-          XOOOOOOOOOOOOX
-          XXXXXXXXXXXXXX
-)
+      expected =
+        ~s(
+                        \N
+                        \N
+   @@@@@                \N
+   @XXX@  XXXXXXXXXXXXXX\N
+   @@@@@  XOOOOOOOOOOOOX\N
+          XOOOOOOOOOOOOX\N
+          XOOOOOOOOOOOOX\N
+          XOOOOOOOOOOOOX\N
+          XXXXXXXXXXXXXX\N)
+        |> prepare_fixture()
 
       assert %{canvas: ^expected} = Render.call(image)
     end
+
+    test "renders second fixture image" do
+      rec1 =
+        build(:rectangle,
+          props: %{
+            corner: [14, 0],
+            width: 7,
+            height: 6,
+            outline: nil,
+            fill: "."
+          }
+        )
+
+      rec2 =
+        build(:rectangle,
+          props: %{
+            corner: [0, 3],
+            width: 8,
+            height: 4,
+            outline: "O",
+            fill: nil
+          }
+        )
+
+      rec3 =
+        build(:rectangle,
+          props: %{
+            corner: [5, 5],
+            width: 5,
+            height: 3,
+            outline: "X",
+            fill: "X"
+          }
+        )
+
+      image = build(:image, width: 20, height: 7, drawings: [rec1, rec2, rec3])
+
+      expected =
+        ~s(
+              .......\N
+              .......\N
+              .......\N
+OOOOOOOO      .......\N
+O      O      .......\N
+O    XXXXX    .......\N
+OOOOOXXXXX           \N
+     XXXXX           \N)
+        |> prepare_fixture()
+
+      assert %{canvas: ^expected} = Render.call(image)
+    end
+  end
+
+  defp prepare_fixture(text) do
+    text
+    |> String.replace("\N", "")
+    |> String.replace("\n", "", global: false)
   end
 end
