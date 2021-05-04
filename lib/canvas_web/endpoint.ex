8e709c32bdee379bc5d4007e5ad47c0e4fcf8a63
vtm9 <vtmilyakov@yandex.ru>
2021-05-04 19:36:01 +0300

    Improve test coverage

--- lib/canvas_web/endpoint.ex
+++ lib/canvas_web/endpoint.ex
@@ -9,21 +9,6 @@ defmodule CanvasWeb.Endpoint do
     key: "_canvas_key",
     signing_salt: "UEbxXY5Y"
   ]
-
-  socket "/socket", CanvasWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :canvas,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
